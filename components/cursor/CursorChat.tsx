61dcec5606cab162f6999a37fa2f8a65507133fe
mnlght03 <evmorvag@gmail.com>
2024-05-12 15:20:35 +0300

    added navbar with avatars

--- components/cursor/CursorChat.tsx
+++ components/cursor/CursorChat.tsx
@@ -43,7 +43,10 @@ const CursorChat = ({
       {cursorState.mode === CursorMode.Chat && (
         <>
           <CursorSVG color='#000' />
-          <div className='absolute left-2 top-5 rounded-[20px] bg-blue-500 px-4 py-2 text-sm leading-relaxed text-white'>
+          <div
+            className='absolute left-2 top-5 rounded-[20px] bg-blue-500 px-4 py-2 text-sm leading-relaxed text-white'
+            onKeyUp={(e) => e.stopPropagation()}
+          >
             {cursorState.previousMessage && (
               <div> {cursorState.previousMessage}</div>
             )}
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,4 @@
-import jsPDF from 'jspdf';
+// import jsPDF from 'jspdf';
 import { twMerge } from 'tailwind-merge';
 import { type ClassValue, clsx } from 'clsx';
 
@@ -39,7 +39,8 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function generateRandomName(): string {
-  const randomAdjective = adjectives[Math.floor(Math.random() * adjectives.length)];
+  const randomAdjective =
+    adjectives[Math.floor(Math.random() * adjectives.length)];
   const randomAnimal = animals[Math.floor(Math.random() * animals.length)];
 
   return `${randomAdjective} ${randomAnimal}`;
@@ -103,11 +104,11 @@ export const exportToPdf = () => {
   if (!canvas) return;
 
   // use jspdf
-  const doc = new jsPDF({
-    orientation: 'landscape',
-    unit: 'px',
-    format: [canvas.width, canvas.height],
-  });
+  // const doc = new jsPDF({
+  // orientation: 'landscape',
+  // unit: 'px',
+  // format: [canvas.width, canvas.height],
+  // });
 
   // get the canvas data url
   const data = canvas.toDataURL();
